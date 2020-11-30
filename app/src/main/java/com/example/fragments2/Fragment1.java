ef994fc2022bf8d2e7113c23b551b96e09f6cd40
Fdrwi <f.drwiish@hotmail.com>
2020-11-30 23:16:17 +0300

    i wrote

--- app/src/main/java/com/example/fragments2/Fragment1.java
+++ app/src/main/java/com/example/fragments2/Fragment1.java
@@ -13,7 +13,7 @@ import android.view.ViewGroup;
  * create an instance of this fragment.
  */
 public class Fragment1 extends Fragment {
-
+// i/m fatimah khalid working now....
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
